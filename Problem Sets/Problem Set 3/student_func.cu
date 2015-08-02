4a32de04fc439da388877d00c4f3e72429cd9cda
Ilya Kavalerov <ilya@artsymail.com>
2015-08-01 23:30:16 -0400

    get shared memory reduce done, next atomicAdd histograming

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -6,7 +6,7 @@
 
   A High Dynamic Range (HDR) image contains a wider variation of intensity
   and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
+  have used in the previous assignment.
 
   To store this extra information we use single precision floating point for
   each channel.  This allows for an extremely wide range of intensity values.
@@ -53,7 +53,7 @@
   Old TV signals used to be transmitted in this way so that black & white
   televisions could display the luminance channel while color televisions would
   display all three of the channels.
-  
+
 
   Tone-mapping
   ============
@@ -81,6 +81,39 @@
 
 #include "utils.h"
 
+#define NUM_THREADS 1024
+
+__global__
+void shmem_min_max_reduce(const float * d_in, float * d_out, const size_t numRows, const size_t numCols, const int maxMode)
+{
+  extern __shared__ float s_in[];
+
+  int tid = threadIdx.x;
+  int offset = blockIdx.x * blockDim.x + tid; // offset in global memory that is
+  int imgSize = numRows * numCols;
+  if (offset >= imgSize)
+    return;
+
+  s_in[tid] = d_in[offset];
+  __syncthreads();
+
+  // use left shift '>>' to divide by 2 each iteration
+  for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+    if (tid < s && (tid + s) < imgSize)
+      if (maxMode) {
+        s_in[tid] = max(s_in[tid], s_in[tid + s]);
+      } else {
+        s_in[tid] = min(s_in[tid], s_in[tid + s]);
+      }
+    __syncthreads();
+  }
+
+  if (tid == 0)
+    d_out[blockIdx.x] = s_in[0];
+}
+
+float *d_min_intermediate[NUM_THREADS], *d_max_intermediate[NUM_THREADS];
+
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   unsigned int* const d_cdf,
                                   float &min_logLum,
@@ -89,16 +122,40 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   const size_t numCols,
                                   const size_t numBins)
 {
-  //TODO
-  /*Here are the steps you need to implement
-    1) find the minimum and maximum value in the input logLuminance channel
-       store in min_logLum and max_logLum
-    2) subtract them to find the range
-    3) generate a histogram of all the values in the logLuminance channel using
-       the formula: bin = (lum[i] - lumMin) / lumRange * numBins
-    4) Perform an exclusive scan (prefix sum) on the histogram to get
-       the cumulative distribution of luminance values (this should go in the
-       incoming d_cdf pointer which already has been allocated for you)       */
+  int blocks = 1 + (numRows*numCols) / NUM_THREADS;
 
+  // 1) find the minimum and maximum value in the input logLuminance channel
+  //    store in min_logLum and max_logLum
+
+  // two kernels, reduction to find min and max
+  // min
+  checkCudaErrors(cudaMalloc(&d_min_intermediate, sizeof(float) * NUM_THREADS));
+  shmem_min_max_reduce<<<blocks, NUM_THREADS, NUM_THREADS * sizeof(float)>>>(d_logLuminance, d_min_intermediate, numRows, numCols, 0)
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  shmem_min_max_reduce<<<1, NUM_THREADS, NUM_THREADS * sizeof(float)>>>(d_logLuminance, min_logLum, numRows, numCols, 0)
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  // max
+  checkCudaErrors(cudaMalloc(&d_max_intermediate, sizeof(float) * NUM_THREADS));
+  shmem_min_max_reduce<<<blocks, NUM_THREADS, NUM_THREADS * sizeof(float)>>>(d_logLuminance, d_max_intermediate, numRows, numCols, 1)
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  shmem_min_max_reduce<<<1, NUM_THREADS, NUM_THREADS * sizeof(float)>>>(d_logLuminance, max_logLum, numRows, numCols, 1)
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  // 2) subtract them to find the range
+  float range = max_logLum - min_logLum;
+
+  // 3) generate a histogram of all the values in the logLuminance channel using
+  //    the formula: bin = (lum[i] - lumMin) / lumRange * numBins
+
+  // first lets try with atomicAdd
+  // later: give every thread local bins, then reduce
+
+  // 4) Perform an exclusive scan (prefix sum) on the histogram to get
+  //    the cumulative distribution of luminance values (this should go in the
+  //    incoming d_cdf pointer which already has been allocated for you)
+
+  // we have 1024 bins, and we have 2880 Thread processors on a K40c, and
+  // 512 on a M2090, so we have plenty of workers, want step efficiency over
+  // work efficiency, will use Hillis & Steele
 
 }

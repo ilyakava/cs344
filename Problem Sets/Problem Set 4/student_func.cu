ac9db3f02b01cafd12733c29fc52ebc7fd8b2d5b
Ilya Kavalerov <ilya@artsymail.com>
2015-08-04 17:23:52 -0400

    have the pointer point to something when declared

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -136,7 +136,7 @@ void your_sort(unsigned int* const d_inputVals,
   unsigned int h_predicateTrue[numElems];
   unsigned int h_predicateTrueScan[numElems];
   unsigned int nsb;
-  unsigned int* h_numPredicateTrueElements;
+  unsigned int* h_numPredicateTrueElements = (unsigned int *)malloc(sizeof(unsigned int));
 
   checkCudaErrors(cudaMalloc((void**)&d_predicate, size));
   checkCudaErrors(cudaMalloc((void**)&d_predicateTrueScan, size));

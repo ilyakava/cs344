729dda48b43de980bf699ea0dd64b3b0981e3f4e
Ilya Kavalerov <ilya@artsymail.com>
2015-08-04 15:35:45 -0400

    ready to run

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -6,7 +6,7 @@
 
 /* Red Eye Removal
    ===============
-   
+
    For this assignment we are implementing red eye removal.  This is
    accomplished by first creating a score for every pixel that tells us how
    likely it is to be a red eye pixel.  We have already done this for you - you
@@ -42,13 +42,126 @@
 
  */
 
+__global__
+void check_bit(unsigned int* const d_inputVals, unsigned int* const d_outputPredicate,
+               const unsigned int bit, const size_t numElems)
+{
+  // this predicate returns TRUE when the significant bit is not present
+  const unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
+  if (id >= numElems)
+    return;
+  int predicate = (d_inputVals[id] & bit) == 0;
+  d_outputPredicate[id] = predicate;
+}
+
+__global__
+void flip_bit(unsigned int* const d_list, const size_t numElems)
+{
+  const unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
+  if (id >= numElems)
+    return;
+  d_list[id] = ((d_list[id] + 1) % 2);
+}
+
+__global__
+void exclusive_blelloch_scan(unsigned int* const d_list, const size_t numElems)
+{
+  const unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
+  if (id >= numElems)
+    return;
+  // reduce
+  unsigned int i;
+  for (i = 2; i <= numElemsl/2; i <<= 1) {
+    if ((id + 1) % i == 0) {
+      unsigned int neighbor_offset = i>>1;
+      d_list[id] += d_list[id - neighbor_offset];
+    }
+    __syncthreads();
+  }
+  i >>= 1; // return i to last value before for loop exited
+  // reset last to identity element
+  if (id == (numElems-1))
+    d_list[id] = 0;
+  // downsweep
+  for (i; i >= 2; i >>= 1) {
+    if((id + 1) % i == 0) {
+      unsigned int neighbor_offset = i>>1;
+      unsigned int old_neighbor = d_list[id - neighbor_offset];
+      d_list[id - neighbor_offset] = d_list[id]; // copy
+      d_list[id] += old_neighbor;
+    }
+    __syncthreads();
+  }
+}
+
+__global__
+void scatter(const unsigned int* const d_input, unsigned int* const d_output,
+             const unsigned int* const d_predicateTrueScan, const unsigned int* const d_predicateFalseScan,
+             const unsigned int* const d_predicateFalse, const unsigned int numPredicateTrueElements,
+             const size_t numElems)
+{
+  const unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
+  if (id >= numElems)
+    return;
+
+  unsigned int newLoc;
+  if (d_predicateFalse == 1)
+    newLoc = d_predicateFalseScan[id] + numPredicateTrueElements;
+  else
+    newLoc = d_predicateTrueScan[id];
+  d_output[newLoc] = d_input[id];
+}
+
+unsigned int* const d_predicate, d_predicateTrueScan, d_predicateFalseScan, ping, pong;
 
 void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_inputPos,
                unsigned int* const d_outputVals,
                unsigned int* const d_outputPos,
                const size_t numElems)
-{ 
-  //TODO
-  //PUT YOUR SORT HERE
+{
+  int size = sizeof(unsigned int) * numElems;
+  int blockSize = 1024;
+  int gridSize = 1 + blockSize / numElems;
+
+  checkCudaErrors(cudaMalloc(&d_predicate, size));
+  checkCudaErrors(cudaMalloc(&d_predicateTrueScan, size));
+  checkCudaErrors(cudaMalloc(&d_predicateFalseScan, size));
+
+  unsigned int lastPredicateTrue, numPredicateTrueElements, nsb;
+
+  for (unsigned int bit = 0; bit <= 32; bit++) {
+    nsb = 1<<bit;
+    // create predicateTrue
+    check_bit<<<gridSize, blockSize>>>(d_inputVals, d_predicate, nsb);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // scan predicateTrue
+    checkCudaErrors(cudaMemcpy(&d_predicateTrueScan, d_predicate, size, cudaMemcpyDeviceToDevice));
+    exclusive_blelloch_scan<<<gridSize, blockSize>>>(d_predicateTrueScan, numElems);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // determine offset of 2nd bin, i.e. how many items are in the 1st bin,
+    // i.e. for how many the predicate is TRUE
+    checkCudaErrors(cudaMemcpy(&lastPredicateTrue, (d_predicate + numElems - 1),
+                               sizeof(unsigned int), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(&numPredicateTrueElements, (d_predicateTrueScan + numElems - 1),
+                               sizeof(unsigned int), cudaMemcpyDeviceToHost));
+    numPredicateTrueElements += lastPredicateTrue;
+    // transform predicateTrue -> predicateFalse
+    flip_bit<<<gridSize, blockSize>>>(d_predicate, numElems);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // scan predicateFalse
+    checkCudaErrors(cudaMemcpy(&d_predicateFalseScan, d_predicate, size, cudaMemcpyDeviceToDevice));
+    exclusive_blelloch_scan<<<gridSize, blockSize>>>(d_predicateFalseScan, numElems);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // scatter values (flip input/output depending on iteration)
+    if ((bit + 1) % 2 == 1) {
+      ping = d_inputVals;
+      pong = d_outputVals;
+    } else {
+      ping = d_outputVals;
+      pong = d_inputVals;
+    }
+    scatter<<<gridSize, blockSize>>>(ping, pong, d_predicateTrueScan, d_predicateFalseScan,
+                                     d_predicateFalse, numPredicateTrueElements, numElems)
+  }
 }

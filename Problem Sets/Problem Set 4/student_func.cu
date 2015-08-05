e9cb4cb670fcc85c26647a3d184d2d48d49b5990
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 10:43:51 -0400

    ready to run the incrementing thing from GPU gems

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -66,41 +66,62 @@ void flip_bit(unsigned int* const d_list, const size_t numElems)
 }
 
 __global__
-void exclusive_blelloch_scan(unsigned int* const d_list, const size_t numElems)
+void partial_exclusive_blelloch_scan(unsigned int* const d_list, unsigned int* const d_block_sums, const size_t numElems)
 {
-  // at the lowest level, we are still working with every other element (hence the 2 *)
-  const unsigned int id = (blockDim.x * blockIdx.x + threadIdx.x);
+  extern __shared__ unsigned int s_block_scan[];
+
+  const unsigned int tid = threadIdx.x;
+  const unsigned int id = blockDim.x * blockIdx.x + tid;
+
+  // copy to shared memory, pad the block that is too small
   if (id >= numElems)
-    return;
+    s_block_scan[tid] = 0;
+  else
+    s_block_scan[tid] = d_list[id];
+  __syncthreads();
 
   // reduce
   unsigned int i;
-  for (i = 2; i <= numElems; i <<= 1) {
-    if ((id + 1) % i == 0) {
+  for (i = 2; i <= blockDim.x; i <<= 1) {
+    if ((tid + 1) % i == 0) {
       unsigned int neighbor_offset = i>>1;
-      d_list[id] += d_list[id - neighbor_offset];
+      s_block_scan[tid] += s_block_scan[tid - neighbor_offset];
     }
     __syncthreads();
   }
   i >>= 1; // return i to last value before for loop exited
-  // reset last to identity element
-  if (id == (numElems-1))
-    d_list[id] = 0;
+  // reset last (sum of whole block) to identity element
+  if (tid == (blockDim.x-1))
+    d_block_sums[blockIdx.x] = s_block_scan[tid];
+    s_block_scan[tid] = 0;
+  __syncthreads();
 
-  // __syncthreads();
   // printf("in kernel: %i %i %i %i %i %i %i %i\n", d_list[0], d_list[1], d_list[2], d_list[3], d_list[4], d_list[5], d_list[6], d_list[7]);
 
   // downsweep
   for (i = i; i >= 2; i >>= 1) {
-    // printf("downsweep: %i\n", i);
-    __syncthreads();
-    if((id + 1) % i == 0) {
+    if((tid + 1) % i == 0) {
       unsigned int neighbor_offset = i>>1;
-      unsigned int old_neighbor = d_list[id - neighbor_offset];
-      d_list[id - neighbor_offset] = d_list[id]; // copy
-      d_list[id] += old_neighbor;
+      unsigned int old_neighbor = s_block_scan[tid - neighbor_offset];
+      s_block_scan[tid - neighbor_offset] = s_block_scan[tid]; // copy
+      s_block_scan[tid] += old_neighbor;
     }
+    __syncthreads();
   }
+
+  // copy result to global memory
+  if (id < numElems)
+    d_list[id] = s_block_scan[tid];
+}
+
+__global__
+void increment_blelloch_scan_with_block_sums(unsigned int* const d_predicateScan, unsigned int* const d_block_sums)
+{
+  const unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
+  if (id >= numElems)
+    return;
+
+  d_predicateScan[id] += d_block_sums[blockIdx.x];
 }
 
 __global__
@@ -132,6 +153,8 @@ unsigned int* d_predicate;
 unsigned int* d_predicateTrueScan;
 unsigned int* d_predicateFalseScan;
 unsigned int* d_numPredicateTrueElements;
+unsigned int* d_numPredicateFalseElements;
+unsigned int* d_block_sums;
 
 // DEBUG
 void print_array(unsigned int* array, size_t length)
@@ -171,6 +194,8 @@ void your_sort(unsigned int* const d_inputVals,
   checkCudaErrors(cudaMalloc((void**)&d_predicateTrueScan, size));
   checkCudaErrors(cudaMalloc((void**)&d_predicateFalseScan, size));
   checkCudaErrors(cudaMalloc((void**)&d_numPredicateTrueElements, sizeof(unsigned int)));
+  checkCudaErrors(cudaMalloc((void**)&d_numPredicateFalseElements, sizeof(unsigned int))); // throwaway
+  checkCudaErrors(cudaMalloc((void**)&d_block_sums, gridSize*sizeof(unsigned int)));
 
   unsigned int max_bits = 30;
   for (unsigned int bit = 0; bit < max_bits; bit++) {
@@ -192,21 +217,24 @@ void your_sort(unsigned int* const d_inputVals,
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
     // scan predicateTrue
     checkCudaErrors(cudaMemcpy(d_predicateTrueScan, d_predicate, size, cudaMemcpyDeviceToDevice));
-    // we halve the number of threads, b/c at most, we are processing every other
-    // element in the array
-    int blelloch_gridSize = (1+(myNumElems / (2*blockSize)));
-    exclusive_blelloch_scan<<<gridSize, blockSize>>>(d_predicateTrueScan, myNumElems);
+    checkCudaErrors(cudaMemset((void**)&d_block_sums, 0, gridSize*sizeof(unsigned int)));
+    partial_exclusive_blelloch_scan<<<gridSize, blockSize, sizeof(unsigned int)*blockSize>>>(d_predicateTrueScan, d_block_sums, myNumElems);
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    partial_exclusive_blelloch_scan<<<1, blockSize, sizeof(unsigned int)*blockSize>>>(d_block_sums, d_numPredicateTrueElements, gridSize);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    increment_blelloch_scan_with_block_sums<<<gridSize, blockSize>>>(d_predicateTrueScan, d_block_sums);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
     // determine offset of 2nd bin, i.e. how many items are in the 1st bin,
     // i.e. for how many the predicate is TRUE
-    checkCudaErrors(cudaMemcpy(&h_predicateTrue, d_predicate,
-                               size, cudaMemcpyDeviceToHost));
-    checkCudaErrors(cudaMemcpy(&h_predicateTrueScan, d_predicateTrueScan,
-                               size, cudaMemcpyDeviceToHost));
-    *h_numPredicateTrueElements = h_predicateTrueScan[myNumElems-1] + h_predicateTrue[myNumElems-1];
-    // printf("h_numPredicateTrueElements: %i\n", *h_numPredicateTrueElements);
-    checkCudaErrors(cudaMemcpy(d_numPredicateTrueElements, h_numPredicateTrueElements,
-                               sizeof(unsigned int), cudaMemcpyHostToDevice));
+    // checkCudaErrors(cudaMemcpy(&h_predicateTrue, d_predicate,
+    //                            size, cudaMemcpyDeviceToHost));
+    // checkCudaErrors(cudaMemcpy(&h_predicateTrueScan, d_predicateTrueScan,
+    //                            size, cudaMemcpyDeviceToHost));
+    // *h_numPredicateTrueElements = h_predicateTrueScan[myNumElems-1] + h_predicateTrue[myNumElems-1];
+    // // printf("h_numPredicateTrueElements: %i\n", *h_numPredicateTrueElements);
+    // checkCudaErrors(cudaMemcpy(d_numPredicateTrueElements, h_numPredicateTrueElements,
+    //                            sizeof(unsigned int), cudaMemcpyHostToDevice));
 
 
     // DEBUG
@@ -222,7 +250,12 @@ void your_sort(unsigned int* const d_inputVals,
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
     // scan predicateFalse
     checkCudaErrors(cudaMemcpy(d_predicateFalseScan, d_predicate, size, cudaMemcpyDeviceToDevice));
-    exclusive_blelloch_scan<<<gridSize, blockSize>>>(d_predicateFalseScan, myNumElems);
+    checkCudaErrors(cudaMemset((void**)&d_block_sums, 0, gridSize*sizeof(unsigned int)));
+    partial_exclusive_blelloch_scan<<<gridSize, blockSize, sizeof(unsigned int)*blockSize>>>(d_predicateFalseScan, d_block_sums, myNumElems);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    partial_exclusive_blelloch_scan<<<1, blockSize, sizeof(unsigned int)*blockSize>>>(d_block_sums, d_numPredicateFalseElements, gridSize);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    increment_blelloch_scan_with_block_sums<<<gridSize, blockSize>>>(d_predicateFalseScan, d_block_sums);
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
     // scatter values (flip input/output depending on iteration)
     if ((bit + 1) % 2 == 1) {

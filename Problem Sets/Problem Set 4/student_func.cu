6c9626a1376250fe2b1aa1e836c7a000d0a7b669
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 11:00:46 -0400

    print blockDim too

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -97,7 +97,7 @@ void partial_exclusive_blelloch_scan(unsigned int* const d_list, unsigned int* c
     s_block_scan[tid] = 0;
   __syncthreads();
 
-  printf("in kernel (thread: %i, block: %i): %i %i %i %i %i %i %i %i\n",tid, blockIdx.x, s_block_scan[0], s_block_scan[1], s_block_scan[2], s_block_scan[3], s_block_scan[4], s_block_scan[5], s_block_scan[6], s_block_scan[7]);
+  printf("in kernel (thread: %i, block: %i of %i): %i %i %i %i %i %i %i %i\n",tid, blockIdx.x, blockDim.x, s_block_scan[0], s_block_scan[1], s_block_scan[2], s_block_scan[3], s_block_scan[4], s_block_scan[5], s_block_scan[6], s_block_scan[7]);
 
   // downsweep
   for (i = i; i >= 2; i >>= 1) {

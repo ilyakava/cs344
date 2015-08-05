34390394ec6320575cc88f7b055824603e68d855
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 11:37:47 -0400

    moving up

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -179,7 +179,7 @@ void your_sort(unsigned int* const d_inputVals,
 
 
   // DEBUG
-  size_t myNumElems = 9;
+  size_t myNumElems = 17;
   int blockSize = 4;
   // printf("myNumElems: %i\n", myNumElems);
 

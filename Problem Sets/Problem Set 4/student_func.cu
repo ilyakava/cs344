f3386d46b1dbe10600d47e4eaba71e4054341d04
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 12:42:07 -0400

    900 by 64

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -180,7 +180,7 @@ void your_sort(unsigned int* const d_inputVals,
 
 
   // DEBUG
-  size_t myNumElems = 950;
+  size_t myNumElems = 900;
   int blockSize = 64;
   // printf("myNumElems: %i\n", myNumElems);
 

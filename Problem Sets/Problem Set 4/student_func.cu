25133f127b9b19c4080ff7c03b5509e16b6c9491
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 11:06:39 -0400

    fill in forgotten curly braces

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -89,16 +89,16 @@ void partial_exclusive_blelloch_scan(unsigned int* const d_list, unsigned int* c
       s_block_scan[tid] += s_block_scan[tid - neighbor_offset];
     }
     __syncthreads();
-    printf("in kernel at i=%i (thread: %i, block: %i of %i): %i %i %i %i %i %i %i %i\n",i,tid, blockIdx.x, blockDim.x, s_block_scan[0], s_block_scan[1], s_block_scan[2], s_block_scan[3], s_block_scan[4], s_block_scan[5], s_block_scan[6], s_block_scan[7]);
   }
   i >>= 1; // return i to last value before for loop exited
   // reset last (sum of whole block) to identity element
-  if (tid == (blockDim.x-1))
+  if (tid == (blockDim.x-1)) {
     d_block_sums[blockIdx.x] = s_block_scan[tid];
     s_block_scan[tid] = 0;
+  }
   __syncthreads();
 
-  // printf("in kernel (thread: %i, block: %i of %i): %i %i %i %i %i %i %i %i\n",tid, blockIdx.x, blockDim.x, s_block_scan[0], s_block_scan[1], s_block_scan[2], s_block_scan[3], s_block_scan[4], s_block_scan[5], s_block_scan[6], s_block_scan[7]);
+  printf("in kernel (thread: %i, block: %i of %i): %i %i %i %i %i %i %i %i\n",tid, blockIdx.x, blockDim.x, s_block_scan[0], s_block_scan[1], s_block_scan[2], s_block_scan[3], s_block_scan[4], s_block_scan[5], s_block_scan[6], s_block_scan[7]);
 
   // downsweep
   for (i = i; i >= 2; i >>= 1) {

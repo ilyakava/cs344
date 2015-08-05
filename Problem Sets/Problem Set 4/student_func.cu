9e65ee96ca7624964e0309a14f5efc28f63127c7
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 12:44:50 -0400

    948 by 64 (949 no good)

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -180,7 +180,7 @@ void your_sort(unsigned int* const d_inputVals,
 
 
   // DEBUG
-  size_t myNumElems = 949;
+  size_t myNumElems = 948;
   int blockSize = 64;
   // printf("myNumElems: %i\n", myNumElems);
 

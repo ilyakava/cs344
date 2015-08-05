0a058d14cb438e1499bd5ef4e81f3ded0785f6d9
Ilya Kavalerov <ilya@artsymail.com>
2015-08-05 11:38:23 -0400

    16 elems

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -179,7 +179,7 @@ void your_sort(unsigned int* const d_inputVals,
 
 
   // DEBUG
-  size_t myNumElems = 17;
+  size_t myNumElems = 16;
   int blockSize = 4;
   // printf("myNumElems: %i\n", myNumElems);
 

3dd29f8a89f8c197f1f9a8412d31332ac7a5bce7
Ilya Kavalerov <ilya@artsymail.com>
2015-08-07 15:11:08 -0400

    test

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -90,23 +90,23 @@ void reduce_on_shmem_first(const unsigned int* const vals, //INPUT
   __syncthreads();
 
   // reduce
-  for (unsigned int ith_hist = 2; ith_hist <= NUM_SHARED_HISTS / 2; ith_hist <<= 1)
-  {
-    unsigned int neighbor_offset = ith_hist>>1;
-    unsigned int neighbor_id = tid - neighbor_offset;
-    if (((tid + 1) % ith_hist) == 0)
-    {
-      for (int i = 0; i < numBins; i++) {
-        s_hists[tid][i] += s_hists[neighbor_id][i];
-      }
-    }
-    __syncthreads();
-  }
+  // for (unsigned int ith_hist = 2; ith_hist <= NUM_SHARED_HISTS / 2; ith_hist <<= 1)
+  // {
+  //   unsigned int neighbor_offset = ith_hist>>1;
+  //   unsigned int neighbor_id = tid - neighbor_offset;
+  //   if (((tid + 1) % ith_hist) == 0)
+  //   {
+  //     for (int i = 0; i < numBins; i++) {
+  //       s_hists[tid][i] += s_hists[neighbor_id][i];
+  //     }
+  //   }
+  //   __syncthreads();
+  // }
 
   // write output
   if (tid == 0) {
     for (int i = 0; i < numBins; i++) {
-      atomicAdd(&histo[i], s_hists[tid][i]);
+      atomicAdd(&histo[i], s_hists[0][i]);
     }
   }
 }

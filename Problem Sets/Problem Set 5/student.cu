916d18b8a8bafacf2c1d5302850cc79d50f24c00
Ilya Kavalerov <ilya@artsymail.com>
2015-08-07 15:15:01 -0400

    not test

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -83,10 +83,10 @@ void reduce_on_shmem_first(const unsigned int* const vals, //INPUT
   for (int i = 0; i < numBins; i++) {
     s_hists[tid][i] = 0;
   }
-  if (id < numElems) {
-    unsigned int bin = vals[id];
-    s_hists[tid][bin] = 1;
-  }
+  // if (id < numElems) {
+  //   unsigned int bin = vals[id];
+  //   s_hists[tid][bin] = 1;
+  // }
   __syncthreads();
 
   // reduce

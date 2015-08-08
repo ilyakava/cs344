09a0de78c51f97c086c656dd103913fd724ff11a
Ilya Kavalerov <ilya@artsymail.com>
2015-08-07 21:40:08 -0400

    try another value of NUM_VALS_PER_THREAD

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -26,7 +26,7 @@
 
 #define NUM_SHARED_HISTS 4
 #define MAX_THREADS_PER_BLOCK 1024
-#define NUM_VALS_PER_THREAD 4
+#define NUM_VALS_PER_THREAD 5
 
 #include "utils.h"
 
@@ -54,6 +54,7 @@ void distribute_atomics_on_shmem_first(const unsigned int* const vals, //INPUT
   // NUM_VALS_PER_THREAD = 1 : 1.2942ms
   // NUM_VALS_PER_THREAD = 2 : 965.25us
   // NUM_VALS_PER_THREAD = 3 : 884.42us
+  // NUM_VALS_PER_THREAD = 4 : 843.81us
 
   extern __shared__ unsigned int s_histo[];
 
